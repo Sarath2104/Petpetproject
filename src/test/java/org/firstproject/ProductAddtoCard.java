6964ffac4c933f1981bd37ea624f3c5df438077f
Sarath2104 <sarathfacer.22@gmail.com>
2023-02-07 14:22:56 +0530

    Updated sarath folder

---
+++ src/test/java/org/firstproject/ProductAddtoCard.java
@@ -0,0 +1,27 @@
+package org.firstproject;
+
+import io.cucumber.java.en.*;
+
+public class ProductAddtoCard {
+	
+	@When("User should click the kids area and select the jumpsuites")
+	public void user_should_click_the_kids_area_and_select_the_jumpsuites() {
+	    
+	}
+
+	@When("User should click the particular product and select the size")
+	public void user_should_click_the_particular_product_and_select_the_size() {
+	    
+	}
+
+	@When("User should click the Add to Bag buttom")
+	public void user_should_click_the_Add_to_Bag_buttom() {
+	    
+	}
+
+	@Then("User should confirm whether the product is added to cart or not")
+	public void user_should_confirm_whether_the_product_is_added_to_cart_or_not() {
+	    
+	}
+
+}
--- src/test/java/org/runner/FirstProjectRunner.java
+++ src/test/java/org/runner/FirstProjectRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions (features="src\\test\\resources\\FeatureFiles\\firstproject-feature", glue="org.firstproject", dryRun=false)
+@CucumberOptions (features="src\\test\\resources\\FeatureFiles\\firstproject-feature", glue="org.firstproject", dryRun=true)
 
 public class FirstProjectRunner {
 
